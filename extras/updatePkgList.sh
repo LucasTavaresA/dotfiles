5e667318058c0f7451f4a71a6e212ac619ba95e5
LucasTavaresA <tavares.lassuncao@gmail.com>
2022-04-20 21:37:52 -0300

    Trocado megasync pelo syncthing

--- extras/updatePkgList.sh
+++ extras/updatePkgList.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-xbps-query -l > "$HOME/extras/voidlinux-pkgs.txt" && exit
-pacman -Qqem > "$HOME/extras/$PC-aurpkgs.txt" && pacman -Qqen > "$HOME/extras/$PC-nativepkgs.txt"
+[ "$PC" = "voidlinux" ] && xbps-query -l > "$HOME/extras/voidlinux-pkgs.txt"
+[ "$PC" = "archlinux" ] && pacman -Qqem > "$HOME/extras/$PC-aurpkgs.txt" && pacman -Qqen > "$HOME/extras/$PC-nativepkgs.txt"
