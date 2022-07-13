f2a1fb8ac3b6ff6928f9459cab7ffed696bcd6c3
LucasTavaresA <tavares.lassuncao@gmail.com>
2022-07-12 21:38:02 -0300

    emacs-init: Trocada ordem de inicialização dos módulos

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -8,15 +8,15 @@
 (add-to-list 'load-path (expand-file-name "modulos/" user-emacs-directory))
 
 (require 'mystraight)
-(require 'mymisc)
-(require 'mygit)
 (require 'mylooks)
 (require 'myedit)
+(require 'mymisc)
+(require 'mygit)
+(require 'mymarkup)
 (require 'mycompletion)
 (require 'mylangs)
-(require 'mymarkup)
-(require 'mykeys)
 (require 'myspell)
+(require 'mykeys)
 
 ;; aumenta pausas para coleta de lixo
 (setq gc-cons-threshold (* 2 1000 1000))
