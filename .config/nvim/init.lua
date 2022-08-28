4d73b55ae1bf803729beca5bb283916445802120
LucasTavaresA <tavares.lassuncao@gmail.com>
2022-08-28 00:24:45 -0300

    nvim: Trocado tema pelo catppuccin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -107,7 +107,10 @@ paq({
     -- "metakirby5/codi.vim";
     -- debug de código
     "puremourning/vimspector";
-    "gavinok/spaceway.vim";
+
+    --- Aparência
+    -- Tema
+    "catppuccin/nvim";
 })
 
 -- carrega plugins locais
@@ -171,9 +174,103 @@ vim.api.nvim_create_autocmd("FileType",
 -- indicação de sintaxe
 vim.cmd("syntax on")
 -- inicia sem a barra - LiteDFM
--- tema - moonlight
-vim.cmd("colorscheme spaceway")
 vim.api.nvim_create_autocmd("VimEnter", { pattern = {"*"}, command = "LiteDFMToggle", })
+-- tema
+vim.g.catppuccin_flavour = "macchiato" -- latte, frappe, macchiato, mocha
+require("catppuccin").setup({
+	dim_inactive = {
+		enabled = true,
+		shade = "dark",
+		percentage = 0.15,
+	},
+	transparent_background = true,
+	term_colors = false,
+	compile = {
+		enabled = false,
+		path = vim.fn.stdpath "cache" .. "/catppuccin",
+	},
+	styles = {
+		comments = { "italic" },
+		conditionals = { "italic" },
+		loops = {},
+		functions = {},
+		keywords = {},
+		strings = {},
+		variables = {},
+		numbers = {},
+		booleans = {},
+		properties = {},
+		types = {},
+		operators = {},
+	},
+	integrations = {
+		treesitter = true,
+		native_lsp = {
+			enabled = true,
+			virtual_text = {
+				errors = { "italic" },
+				hints = { "italic" },
+				warnings = { "italic" },
+				information = { "italic" },
+			},
+			underlines = {
+				errors = { "underline" },
+				hints = { "underline" },
+				warnings = { "underline" },
+				information = { "underline" },
+			},
+		},
+		coc_nvim = false,
+		lsp_trouble = false,
+		cmp = true,
+		lsp_saga = false,
+		gitgutter = false,
+		gitsigns = true,
+		leap = false,
+		telescope = true,
+		nvimtree = true,
+		neotree = {
+			enabled = false,
+			show_root = true,
+			transparent_panel = false,
+		},
+		dap = {
+			enabled = false,
+			enable_ui = false,
+		},
+		which_key = false,
+		indent_blankline = {
+			enabled = true,
+			colored_indent_levels = false,
+		},
+		dashboard = true,
+		neogit = false,
+		vim_sneak = false,
+		fern = false,
+		barbar = false,
+		bufferline = {
+			enabled = true,
+			italics = true,
+			bolds = true,
+		},
+		markdown = true,
+		lightspeed = false,
+		ts_rainbow = false,
+		hop = false,
+		notify = true,
+		telekasten = true,
+		symbols_outline = true,
+		mini = false,
+		aerial = false,
+		vimwiki = true,
+		beacon = true,
+		navic = false,
+		overseer = false,
+	},
+	color_overrides = {},
+	highlight_overrides = {},
+})
+vim.cmd [[colorscheme catppuccin]]
 -- tema do visual multi
 vim.g.VM_theme = "neon"
 -- indica linha selecionada no modo normal
