e9f6462ce10444d131af408a9d5f69244b341fc4
LucasTavaresA <tavares.lassuncao@gmail.com>
2022-09-15 21:26:59 -0300

    emacs git: Adicionado advice para gerenciar dotfiles

--- .config/emacs/modulos/mygit.el
+++ .config/emacs/modulos/mygit.el
@@ -13,6 +13,19 @@
 (use-package magit-delta
   :hook (magit-mode . magit-delta-mode))
 
+(defun magit-dotfiles (env)
+  "Adiciona GIT_DIR e GIT_WORK_TREE ao ENV quando na $HOME.
+https://github.com/magit/magit/issues/460 (@cpitclaudel)."
+  (let ((default (file-name-as-directory (expand-file-name default-directory)))
+        (home (expand-file-name "~/")))
+    (when (string= default home)
+      (let ((gitdir (expand-file-name "~/.dotfiles/")))
+        (push (format "GIT_WORK_TREE=%s" home) env)
+        (push (format "GIT_DIR=%s" gitdir) env))))
+  env)
+
+(advice-add 'magit-process-environment :filter-return #'magit-dotfiles)
+
 ;;; Github
 (use-package forge
   :after (magit))
