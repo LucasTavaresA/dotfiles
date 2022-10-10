2aea9e30839db258a438b78a545c151c6cee7299
LucasTavaresA <tavares.lassuncao@gmail.com>
2022-10-10 17:42:29 -0300

    nvim teclas: Atualizadas todas as teclas

--- .config/nvim/lua/teclas.lua
+++ .config/nvim/lua/teclas.lua
@@ -5,14 +5,22 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 -- cancela indicação de palavras procuradas
-keymap("n", "<esc><esc>", ":noh<CR>", {})
--- colar na linha abaixo
-keymap("n", "P", ":norm o<CR>p", {})
--- ativa/desativa o corretor ortográfico
-keymap("n", "tt", ":setlocal spell! spelllang=pt<CR>", {})
-keymap("n", "te", ":setlocal spell! spelllang=en<CR>", {})
+keymap("n", "<esc>", ":noh<CR>", {})
+-- colar na linha abaixo/acima
+keymap("n", "p", ":norm o<CR>p", opts)
+keymap("n", "P", ":norm O<CR>p", {})
+-- trocar de buffer
+keymap("n", "<C-Tab>", ":bn<CR>", {})
+-- trocar de split
+keymap("n", "<A-Tab>", "<C-w>w", {})
+-- formatar buffer
+keymap("n", "<leader>F", "gg=G", {})
+-- formatar paragrafo
+keymap("n", "<leader>ii", "{=}", {})
+-- alinhar texto
+keymap("v", "<leader>a", ":'<,'>!column -t -o ' '<CR>", {})
 -- mudar o typo de arquivo
-keymap("n", "ft", ":set filetype=", {})
+keymap("n", "<leader>ft", ":set filetype=", {})
 -- salvar buffer
 keymap("n", "<leader>ww", ":w<CR>", {})
 -- sair e salvar
@@ -21,38 +29,48 @@ keymap("n", "<leader>wq", ":wq!<CR>", {})
 keymap("n", "<leader>qq", ":q!<CR>", {})
 -- salvar e recarregar arquivo
 keymap("n", "<leader>wr", ":w<CR>:e<CR>", {})
--- abre ultima mensagem
+-- deletar buffer
+keymap("n", "<leader>k", ":bd<CR>", {})
+-- abre o buffer de mensagems
 keymap("n", "<leader>m", ":message<CR>", {})
 -- divide a tela do lado
 keymap("n", "<C-A-Right>", ":vs<CR>", {})
 -- divide a tela abaixo
 keymap("n", "<C-A-Down>", ":sp<CR>", {})
 -- copiar buffer
-keymap("n", "cb", "ggVGy", { noremap = true })
+keymap("n", "<leader>bc", "ggVGy", { noremap = true })
 -- ativa/desativa números de linha
 keymap("n", "zn", ":set number!<CR>", {})
+-- ativa/desativa indicação de linha
+keymap("n", "zl", ":set cursorline!<CR>", {})
+-- ativa/desativa o corretor ortográfico
+keymap("n", "zs", ":setlocal spell! spelllang=pt<CR>", {})
+keymap("n", "zS", ":setlocal spell! spelllang=en<CR>", {})
 -- procura palavra no cursor
 keymap("n", "?", "*", {})
 -- abre terminal no local do arquivo atual
 keymap("n", "<leader><return>", ":!sh -c 'cd %:p:h ; term_open' &<CR><CR>", {})
+-- cria um macro
+keymap("n", "M", "q", opts)
+keymap("n", "q", "", {})
 -- executa um macro
 keymap("n", "m", "@", {})
 -- abre/fecha fold
 keymap("n", "zz", "za", {})
 -- marca/desmarca caixas
 vim.cmd([[
-function Marcar()
-    let l:line=getline('.')
-    let l:curs=winsaveview()
-    if l:line=~?'\s*-\s*\[\s*\].*'
-        s/\[.\]/[X]/
-    elseif l:line=~?'\s*-\s*\[X\].*'
-        s/\[X\]/[ ]/
-    endif
-    call winrestview(l:curs)
-endfunction
-autocmd FileType markdown nnoremap <silent> zx :call Marcar()<CR>j
-autocmd FileType org nnoremap <silent> zx :call Marcar()<CR>j
+    function Marcar()
+        let l:line=getline('.')
+        let l:curs=winsaveview()
+        if l:line=~?'\s*-\s*\[\s*\].*'
+            s/\[.\]/[X]/
+        elseif l:line=~?'\s*-\s*\[X\].*'
+            s/\[X\]/[ ]/
+        endif
+        call winrestview(l:curs)
+    endfunction
+    autocmd FileType markdown nnoremap <silent> zx :call Marcar()<CR>j
+    autocmd FileType org nnoremap <silent> zx :call Marcar()<CR>j
 ]])
 
 --- Plugins
@@ -78,23 +96,21 @@ keymap("v", "<A-Up>", "<Plug>MoveBlockUp", {})
 keymap("n", "<C-s>", "<C-n>", {})
 keymap("v", "<C-s>", "<C-n>", {})
 -- criar cursor abaixo - visual multi
-keymap("n", "<A-s>", "<C-Down>", {})
+keymap("n", "<C-S-s>", "<C-Down>", {})
 -- procura linhas no buffer - swoop
 keymap("n", ";", ":call Swoop()<CR>", {})
 keymap("v", ";", ":call SwoopSelection()<CR>", {})
 -- abre arquivos no repositório atual - ctrlp
-keymap("n", "ff", ":CtrlP<CR>", {})
+keymap("n", "<leader>ff", ":CtrlP<CR>", {})
 -- abre arquivos abertos recentemente - ctrlp
 keymap("n", "<leader><leader>", ":CtrlPMRUFiles<CR>", {})
--- trocar de buffer
-keymap("n", "<C-Tab>", ":bn<CR>", {})
 -- abre arquivos na home - fzf
-keymap("n", "fh", ":Files ~<CR>", {})
+keymap("n", "<leader>fh", ":Files ~<CR>", {})
 -- comentar linhas - vim comentary
 keymap("n", ",,", "gccj", {})
 keymap("v", ",,", "gc", {})
 -- ativa previsão de cores - nvimcolorizer
-keymap("n", "zr", ":ColorizerToggle<CR>", {})
+keymap("n", "zc", ":ColorizerToggle<CR>", {})
 -- abrir e fechar arvore de undos - undotree
 keymap("n", "zu", ":UndotreeToggle<CR>:UndotreeFocus<CR>", {})
 -- editar snippets para o tipo de arquivo atual - ultisnips
@@ -114,17 +130,14 @@ On_attach = function(_, bufnr)
     buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
 
     opts = { buffer = bufnr, noremap = true, silent = true }
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
     vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
     vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
     vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-    vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
     vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
     vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-    vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
-    vim.keymap.set('n', '<space>ii', function() vim.lsp.buf.format { async = true } end, opts)
+    vim.keymap.set('n', '<leader>D', vim.diagnostic.setloclist, opts)
+    vim.keymap.set('n', '<leader>F', function() vim.lsp.buf.format { async = true } end, opts)
+    vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, opts)
 end
