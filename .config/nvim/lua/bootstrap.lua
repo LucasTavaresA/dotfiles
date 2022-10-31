81b091e4928d265f26f88856edef01519a786ab3
LucasTavaresA <tavares.lassuncao@gmail.com>
2022-10-30 21:37:40 -0300

    nvim: Gitsigns usa numero de linhas

--- .config/nvim/lua/bootstrap.lua
+++ .config/nvim/lua/bootstrap.lua
@@ -375,6 +375,10 @@ return require("packer").startup(function(use)
     "lewis6991/gitsigns.nvim",
     config = function()
       require("gitsigns").setup({
+        signcolumn = false, -- Toggle with `:Gitsigns toggle_signs`
+        numhl = true, -- Toggle with `:Gitsigns toggle_numhl`
+        linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
+        word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
         worktrees = {
           {
             toplevel = vim.env.HOME,
