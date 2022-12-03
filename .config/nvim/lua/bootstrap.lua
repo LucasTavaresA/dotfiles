74a3ef54813b5daf4105989d5238ad66fd81b9a1
LucasTavaresA <tavares.lassuncao@gmail.com>
2022-12-03 06:32:04 -0300

    nvim(treesitter): Desativa highlighting em arquivos grandes

--- .config/nvim/lua/bootstrap.lua
+++ .config/nvim/lua/bootstrap.lua
@@ -451,6 +451,14 @@ return require("packer").startup(function(use)
         },
         highlight = { -- Indicação de sintaxe
           enable = true,
+          disable = function(lang, buf)
+            local max_filesize = 100 * 1024 -- 100 KB
+            local ok, stats =
+              pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+            if ok and stats and stats.size > max_filesize then
+              return true
+            end
+          end,
         },
         incremental_selection = {
           enable = true,
