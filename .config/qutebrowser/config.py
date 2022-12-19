acf2ec1456bbec2a3da13521035becabce10c9fa
LucasTavaresA <tavares.lassuncao@gmail.com>
2022-12-19 08:51:31 -0300

    qutebrowser(config): Ignora erros flake8 e pylint

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,3 +1,12 @@
+# pylint: disable=C0111
+c = c  # noqa: F821 pylint: disable=E0602,C0103
+config = config  # noqa: F821 pylint: disable=E0602,C0103
+# pylint: disable=C0111
+from qutebrowser.config.configfiles import ConfigAPI  # noqa: F401
+from qutebrowser.config.config import ConfigContainer  # noqa: F401
+config: ConfigAPI = config  # noqa: F821 pylint: disable=E0602,C0103
+c: ConfigContainer = c  # noqa: F821 pylint: disable=E0602,C0103
+
 # carregar o autoconfig.yml
 config.load_autoconfig(False)
 
