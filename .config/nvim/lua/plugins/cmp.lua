b95ee951c024f9053202f0ab4a367b4ef10ba39e
LucasTavaresA <tavares.lassuncao@gmail.com>
2023-01-03 00:22:44 -0300

    nvim(lsp-kind): Consertado concatenating by nil value

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -8,6 +8,35 @@ return {
       "hrsh7th/cmp-path",
       "dcampos/cmp-snippy",
       "f3fora/cmp-spell",
+      {
+        "onsails/lspkind.nvim",
+        config = function()
+          require("cmp").setup({
+            experimental = { ghost_text = true },
+            window = {
+              completion = {
+                winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
+                col_offset = -3,
+                side_padding = 0,
+              },
+            },
+            formatting = {
+              fields = { "kind", "abbr", "menu" },
+              format = function(entry, vim_item)
+                local kind = require("lspkind").cmp_format({
+                  mode = "symbol_text",
+                  maxwidth = 25,
+                  ellipsis_char = "…", -- when pop-up menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+                })(entry, vim_item)
+                local strings = vim.split(kind.kind, "%s", { trimempty = true })
+                kind.kind = " " .. (strings[1] or "") .. " "
+                kind.menu = "    (" .. (strings[2] or "") .. ")"
+                return kind
+              end,
+            },
+          })
+        end,
+      },
     },
     config = function()
       local has_words_before = function()
