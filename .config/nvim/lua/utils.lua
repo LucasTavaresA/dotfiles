e622035ab5d256679d8d15c20937c45f0b4d30de
LucasTavaresA <tavares.lassuncao@gmail.com>
2023-02-02 01:37:05 -0300

    nvim(utils): Adicionado comando redir

--- .config/nvim/lua/utils.lua
+++ .config/nvim/lua/utils.lua
@@ -182,3 +182,12 @@ function ReplaceSel()
       .. '")'
   )
 end
+
+--- places command output in a buffer
+vim.api.nvim_create_user_command("Redir", function(ctx)
+  local lines =
+    vim.split(vim.api.nvim_exec(ctx.args, true), "\n", { plain = true })
+  vim.cmd("new")
+  vim.api.nvim_buf_set_lines(0, 0, -1, false, lines)
+  vim.opt_local.modified = false
+end, { nargs = "+", complete = "command" })
