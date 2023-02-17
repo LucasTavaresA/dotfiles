5fc1cb6bbd76497dc0e69deb28cc86c031d3612b
LucasTavaresA <tavares.lassuncao@gmail.com>
2023-02-17 00:58:50 -0300

    nvim(treesitter): Adicionado tsnode-marker

--- .config/nvim/lua/plugins/tree-sitter.lua
+++ .config/nvim/lua/plugins/tree-sitter.lua
@@ -361,4 +361,22 @@ return {
     ft = "html",
     dependencies = "nvim-treesitter/nvim-treesitter",
   },
+  {
+    "atusy/tsnode-marker.nvim",
+    lazy = true,
+    dependencies = "nvim-treesitter/nvim-treesitter",
+    filetype = "markdown",
+    init = function()
+      vim.api.nvim_create_autocmd("FileType", {
+        group = vim.api.nvim_create_augroup("tsnode-marker-markdown", {}),
+        pattern = "markdown",
+        callback = function(ctx)
+          require("tsnode-marker").set_automark(ctx.buf, {
+            target = { "code_fence_content" },
+            hl_group = "CursorLine",
+          })
+        end,
+      })
+    end,
+  },
 }
