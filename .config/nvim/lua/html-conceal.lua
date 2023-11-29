84bc7abcc555dfdc0410946ad1ab01b3e1fa5817
LucasTavaresA <tavares.lassuncao@gmail.com>
2023-11-29 19:30:48 -0300

    nvim(html-conceal): Update to vim.treesitter.query.parse()

--- .config/nvim/lua/html-conceal.lua
+++ .config/nvim/lua/html-conceal.lua
@@ -12,24 +12,29 @@ local conceal_html_class = function(bufnr)
 	local syntax_tree = language_tree:parse()
 	local root = syntax_tree[1]:root()
 
-	local query = vim.treesitter.parse_query(
-		"html",
-		[[
-    ((attribute
-        (attribute_name) @att_name (#eq? @att_name "class")
-        (quoted_attribute_value (attribute_value) @class_value) (#set! @class_value conceal "…")))
-    ]]
-	) -- using single character for conceal thanks to u/Rafat913
+	local query = [[
+        ((attribute
+          (attribute_name) @att_name (#eq? @att_name "class")
+          (quoted_attribute_value (attribute_value) @class_value) (#set! @class_value conceal "…")))
+        ]]
+
+	local ok, ts_query = pcall(vim.treesitter.query.parse, "html", query)
+	if not ok then
+		return
+	end
 
 	for _, captures, metadata in
-		query:iter_matches(root, bufnr, root:start(), root:end_())
+	ts_query:iter_matches(root, bufnr, root:start(), root:end_(), {})
 	do
 		local start_row, start_col, end_row, end_col = captures[2]:range()
-		vim.api.nvim_buf_set_extmark(bufnr, namespace, start_row, start_col, {
-			end_line = end_row,
-			end_col = end_col,
-			conceal = metadata[2].conceal,
-		})
+		-- This conditional prevents conceal leakage if the class attribute is erroneously formed
+		if (end_row - start_row) == 0 then
+			vim.api.nvim_buf_set_extmark(bufnr, namespace, start_row, start_col, {
+				end_line = end_row,
+				end_col = end_col,
+				conceal = metadata[2].conceal,
+			})
+		end
 	end
 end
 
